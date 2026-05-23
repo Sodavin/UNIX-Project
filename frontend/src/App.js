5b8b71f077a881c96bcc8f07ce02936d58f2cf7d
Preap Sodavin <sodavinpreap@gmail.com>
2026-05-24 01:52:32 +0700

    Fixed frontend folder tracking

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios.get("http://127.0.0.1:8000/api/")
+      .then(response => {
+        setMessage(response.data.message);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default App;
